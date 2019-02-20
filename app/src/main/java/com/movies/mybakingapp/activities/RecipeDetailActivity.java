56fa8565939b08e2b434a99c032d2d083dfff55e
VargaJohanna <varga.johanna@gmail.com>
2019-02-20 21:18:56 +0000

    Prepare layouts for recipe detail activity

---
+++ app/src/main/java/com/movies/mybakingapp/activities/RecipeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.movies.mybakingapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.movies.mybakingapp.R;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
