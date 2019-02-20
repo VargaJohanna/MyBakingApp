f8e1d7bf9d499880269a813422017bf76606e236
VargaJohanna <varga.johanna@gmail.com>
2019-02-20 13:21:27 +0000

    Initial commit

---
+++ app/src/main/java/com/movies/mybakingapp/MainRecipeListActivity.java
@@ -0,0 +1,13 @@
+package com.movies.mybakingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainRecipeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_recipe_list);
+    }
+}
