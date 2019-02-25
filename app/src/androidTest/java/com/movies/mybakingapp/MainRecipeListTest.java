77b14d6fb8f8571d88167c9728927d3738f71d67
VargaJohanna <varga.johanna@gmail.com>
2019-02-25 10:16:57 +0000

    Add more checks to the test

--- app/src/androidTest/java/com/movies/mybakingapp/MainRecipeListTest.java
+++ app/src/androidTest/java/com/movies/mybakingapp/MainRecipeListTest.java
@@ -14,6 +14,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
@@ -27,19 +28,20 @@ public class MainRecipeListTest {
     @Rule
     public ActivityTestRule<MainRecipeListActivity> mainRecipeListTestRule = new ActivityTestRule<>(MainRecipeListActivity.class);
 
-//    @Before
-//    public void init(){
-//        mainRecipeListTestRule.getActivity().getSupportFragmentManager().beginTransaction();
-//    }
-
     @Test
-    public void clickListItem_OpensRecipeInstrictionsActivity() {
+    public void clickFirstRecipeInList_OpensRecipeInstructionsActivity() {
         // Click on the first item in the list
         onView(withId(R.id.recipe_recycle_list)).perform(actionOnItemAtPosition(0, click()));
 
+        // Assert action bar title
         onView(allOf(instanceOf(TextView.class),
                 withParent(withResourceName("action_bar"))))
                 .check(matches(withText("Nutella Pie")));
+
+        onView(withId(R.id.ingredients_title)).check(matches(withText("Ingredients:")));
+        onView(withId(R.id.ingredients_recycler_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.how_to_make_it_title)).check(matches(withText("How to make it:")));
+        onView(withId(R.id.steps_recycler_view)).check(matches(isDisplayed()));
     }
 
 }
